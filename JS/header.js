af5f7040908678a879932f1b09262144c9569b89
SugitaniShouta <infinity071802@gmail.com>
2026-02-26 22:42:50 +0900

    header swiper JS

---
+++ JS/header.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 4000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
