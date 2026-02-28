d6ccb2ddfa532734dc99d6dad91ec39324563b09
SugitaniShouta <infinity071802@gmail.com>
2026-02-28 19:19:18 +0900

    contact modal JS

--- JS/LP.js
+++ JS/LP.js
@@ -14,3 +14,27 @@ function tellTime(){
   const date = new Date();
   alert(date);
 }
+
+const modal = document.querySelector('.modal-blackBG');
+const body = document.querySelector('.body');
+const Name = document.getElementById("modal-name");
+const Email = document.getElementById("modal-email");
+const About = document.getElementById("modal-about");
+const Content = document.getElementById("modal-content");
+function openModal() {
+  modal.classList.add("modal-open");
+  body.classList.add("modal-open");
+
+  const formName = document.getElementById("contact-name").value;
+  const formEmail = document.getElementById("contact-email").value;
+  const formAbout = document.getElementById("contact-about").value;
+  const formContent = document.getElementById("contact-content").value;
+  Name.textContent = formName
+  Email.textContent = formEmail
+  About.textContent = formAbout
+  Content.textContent = formContent
+}
+function closeModal() {
+  modal.classList.remove("modal-open");
+  body.classList.remove("modal-open");
+}
