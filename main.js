2cfa00a622466a96ffeb7a489a665b88e09d4428
rettu25252 <ishirettuu@gmail.com>
2026-03-02 00:03:28 +0900

    課題4-1完了

---
+++ main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoHeight: false,
+  alignItems: 'stretch',
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false, // ユーザーが手動でスライドを触っても、自動再生を止めない設定
+  },
+});
