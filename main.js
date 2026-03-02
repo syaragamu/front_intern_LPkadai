d05224ebb9ce7fc5736f5fd4baacf263a0cf24aa
rettu25252 <ishirettuu@gmail.com>
2026-03-02 18:46:40 +0900

    課題4-4完了

--- main.js
+++ main.js
@@ -20,3 +20,35 @@ const btn = document.getElementById('Btn');
 btn.addEventListener('click', function() {
   alert(Date());
 });
+
+const modalArea = document.getElementById('modalArea');
+const submitBeforeBtn = document.getElementById('submit_before');
+const cancelBtn = document.getElementById('cancel');
+const submitBtn = document.getElementById('submit');
+const body = document.body;
+
+const Name = document.getElementById('name');
+const modalName = document.getElementById('modalName');
+const email = document.getElementById('email');
+const modalEmail = document.getElementById('modalEmail');
+const title = document.getElementById('title');
+const modalTitle = document.getElementById('modalTitle');
+const content = document.getElementById('content');
+const modalContent = document.getElementById('modalContent');
+
+submitBeforeBtn.addEventListener('click', function() {
+    modalName.textContent = Name.value;
+    modalEmail.textContent = email.value;
+    modalTitle.textContent = title.options[title.selectedIndex].text;
+    modalContent.textContent = content.value;
+    modalArea.style.display = "block";
+    body.classList.add('is-modal-open');
+});
+cancelBtn.addEventListener('click', function() {
+    modalArea.style.display = "none";
+    body.classList.remove('is-modal-open');
+});
+submitBtn.addEventListener('click', function() {
+    modalArea.style.display = "none";
+    body.classList.remove('is-modal-open');
+});
